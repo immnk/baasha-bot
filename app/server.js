483f42448e59f55115cc004a1672bd987715321b
Mani <im.manikanta@gmail.com>
2017-05-05 20:50:04 +0530

    change port number

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ var constants = require('./modules/constants');
 var fbMessenger = require('./modules/fbMessenger');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // Process application/json
