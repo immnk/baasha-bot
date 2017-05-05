42e04f76e0c431cd474c8f511b299c810e6b1f4c
Mani <im.manikanta@gmail.com>
2017-05-05 20:47:18 +0530

    Node services

---
+++ app/modules/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "PAGE_ACCESS_TOKEN": "EAACw1l6qxqsBAJsKETz7nwSRMesdxqi9YF1CZAfxYbUkTe5V5fp7Ada2dER4gBhNGY8mHS2ltOcTsYUwrGef5LGjipKjp8BdZCDYrZAQHZCYVNUJlezm8P9gBMPEmWI28lDvDWVKYTC1FekZCEIhkvxjaqhYkz0Y9jZB1xdhgxZAwZDZD",
+    "FB_MESSAGES_URL": "https://graph.facebook.com/v2.6/me/messages",
+    "WIT_AI_ENDPOINT": "https://api.wit.ai/message?v=20160829&q=",
+    "WIT_AI_SERVER_TOKEN": "Bearer BFYEQAC2CHDMSNJY6WI7G7OOA5BLPMD4",
+    "SERVER_URL": "https://thebotdesigners.herokuapp.com",
+    "HTML_DIR": "WebContent/html/",
+    "DB_URL": "mongodb://sirius:avengers@ds017256.mlab.com:17256/users",
+};
