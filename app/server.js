6fe324a126d0d90235969608b24971962b1e6782
meenakship <meenakship@192.168.16.216>
2017-05-06 04:07:33 +0530

    FD TIckets

--- app/server.js
+++ app/server.js
@@ -6,6 +6,10 @@ var fbMessenger = require('./modules/fbMessenger');
 var mongoose = require('mongoose');
 var config = require('./config');
 global.__base = __dirname + '/';
+var FD_API_KEY = "USyBH4BxWXMHNlsuvxh3";
+var FD_ENDPOINT = "meesun";
+var https = require("https");
+var unirest = require('unirest');
 
 var app = express();
 
@@ -28,6 +32,18 @@ var theatre = require(__dirname + '/routes/theatre')();
 // Index route
 app.get('/', function(req, res) {
     res.send('Hello world, I am a chat bot')
+});
+app.get('/createTicket', function(req,res){
+	createFDTicket(req,res);
+	
+});
+
+app.get('/getTicketStatus', function(req,res){
+	getFDTicketStatus(req,res);
+});
+
+app.get('/pushOnResolution', function(req,res){
+	res.send('ticket resolved')
 });
 
 app.get('/login', function(req, res) {
@@ -86,7 +102,91 @@ app.post('/webhook/', function(req, res) {
         res.sendStatus(200);
     }
 });
+function createFDTicket(req,res)
+{
+	var PATH = "/api/v2/tickets";
+	var URL =  "https://" + FD_ENDPOINT + ".freshdesk.com"+ PATH;
 
+	var fields = {
+	  'email': req.query.emailId,
+	  'subject': req.query.subject,
+	  'description': req.query.description,
+	  'status': 2,
+	  'priority': 1
+	}
+
+	var Request = unirest.post(URL);
+
+	Request.auth({
+	  user: FD_API_KEY,
+	  pass: "Ms-4113009",
+	  sendImmediately: true
+	})
+	.type('json')
+	.send(fields)
+	.end(function(response){
+	  console.log(response.body)
+	  
+	  console.log("Response Status : " + response.status)
+	  
+	  if(response.status == 201){
+		var ticket = require(__base + 'models/tickets');
+		var ticketSchema = new ticket({
+			  userId: req.query.userId,
+			  ticketId: response.body.id,
+			  description: req.query.description,
+			  email: req.query.emailId,
+			  subject: req.query.subject
+			});
+		ticketSchema.save(function(err) {
+			if (err) next(err);
+
+			console.log('tickets persisted in db!');
+		});
+		res.send('Ticket created successfully. Ticket id is '+response.body.id)
+	    console.log("Location Header : "+ response.headers['location'])
+	  }
+	  else{
+	    	console.log("X-Request-Id :" + response.headers['x-request-id']);
+	  }
+	  });
+}
+
+function getFDTicketStatus(req,res){
+	var PATH = "/api/v2/tickets/"+req.query.id;
+	var URL =  "https://" + FD_ENDPOINT + ".freshdesk.com"+ PATH;
+	var Request = unirest.get(URL);
+
+	Request.auth({
+	  user: FD_API_KEY,
+	  pass: "Ms-4113009",
+	  sendImmediately: true
+	})
+	.type('json')
+	.end(function(response){
+	  console.log(response.body)
+	  var status=response.body.status;
+	  var statusTxt;
+	  switch(status)
+	  {
+	  case 2:
+		  statusTxt='Open';
+		  break;
+	  case 3:
+		  statusTxt="Pending";
+		  break;
+	  case 4:
+		  statusTxt="Resolved";
+		  break;
+	  case 5:
+		  statusTxt="Closed";
+		  break;
+	  }
+	  res.send(statusTxt);
+	  console.log("Ticket Status Response Status : " + response.body.status)
+	  
+	  });
+}
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
