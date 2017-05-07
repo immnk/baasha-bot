367bf2ce889b3502e128156a4fdb47b3e1763f14
Mani <im.manikanta@gmail.com>
2017-05-07 16:11:35 +0530

    Remove message

--- app/server.js
+++ app/server.js
@@ -156,7 +156,7 @@ var myJob = new cronJob('5 * * * * *', function() {
                                 });
                             }
                         });
-                        sendTextMessage(global.__senderId, "Issue has been resolved! #" + global.__ticketId);
+                        // sendTextMessage(global.__senderId, "Issue has been resolved! #" + global.__ticketId);
                     }
                 });
             }
